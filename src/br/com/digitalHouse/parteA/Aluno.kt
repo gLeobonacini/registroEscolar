dd4a0baff8be8ad2d7117399c6f0bd899801d3d4
gLeobonacini <70539330+gLeobonacini@users.noreply.github.com>
2020-09-25 15:10:19 -0300

    Subindo Parte B

--- src/br/com/digitalHouse/parteA/Aluno.kt
+++ src/br/com/digitalHouse/parteA/Aluno.kt
@@ -1,6 +1,7 @@
 package br.com.digitalHouse.parteA
 
 data class Aluno(val nome: String, val sobrenome: String, val codigoDeAluno: Int) {
+    // Alterando função equals para comparar apenas codigoDeAluno
     override fun equals(other: Any?): Boolean {
         if(other == null || other !is Aluno)
             return false
---
+++ src/br/com/digitalHouse/parteB/Curso.kt
@@ -0,0 +1,11 @@
+package br.com.digitalHouse.parteB
+
+class Curso(val nome: String, val codigoDeCurso: Int) {
+    // Alterando função equals para comparar apenas codigoDeCurso
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is Curso)
+            return false
+        else
+            return this.codigoDeCurso == other.codigoDeCurso
+    }
+}
