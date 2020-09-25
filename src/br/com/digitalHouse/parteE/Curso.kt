6afded713301e5389f561fb7b4f45ec4401a7327
gLeobonacini <70539330+gLeobonacini@users.noreply.github.com>
2020-09-25 15:57:04 -0300

    Subindo Parte E

---
+++ src/br/com/digitalHouse/parteE/Curso.kt
@@ -0,0 +1,16 @@
+package br.com.digitalHouse.parteE
+
+class Curso(val nome: String,
+            val codigoDeCurso: Int,
+            val professorTitular: ProfessorTitular,
+            val professorAdjunto: ProfessorAdjunto,
+            val quantidadeMaximaDeAlunos: Int,
+            val listaDeAlunos: MutableList<Aluno>) {
+    // Alterando função equals para comparar apenas codigoDeCurso
+    override fun equals(other: Any?): Boolean {
+        if(other == null || other !is Curso)
+            return false
+        else
+            return this.codigoDeCurso == other.codigoDeCurso
+    }
+}
